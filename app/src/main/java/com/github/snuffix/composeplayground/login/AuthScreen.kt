4c18d2c53be67b581a05055e57a85b1122df3733
Adrian Kremski <adrian@treelineinteractive.com>
2024-06-12 19:49:35 +0200

    Another update to auth screen example

--- app/src/main/java/com/github/snuffix/composeplayground/login/AuthScreen.kt
+++ app/src/main/java/com/github/snuffix/composeplayground/login/AuthScreen.kt
@@ -1,17 +1,22 @@
 package com.github.snuffix.composeplayground.login
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -19,6 +24,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -27,6 +33,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
@@ -35,9 +42,12 @@ import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.positionInParent
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.lerp
+import androidx.compose.ui.unit.sp
 import com.github.snuffix.composeplayground.calculatePointAngleOnCircle
 import kotlin.math.abs
 import kotlin.math.cos
@@ -78,17 +88,23 @@ fun AuthScreen() {
 
     var transitionToRegisterScreen by remember { mutableStateOf(false) }
     var registrationScreenVisible by remember { mutableStateOf(false) }
+    var registrationFormVisible by remember { mutableStateOf(false) }
+    var loginFormVisible by remember { mutableStateOf(true) }
 
     val registerButtonTransitionAnim = remember { Animatable(0f) }
     val closeButtonTransitionAnim = remember { Animatable(0f) }
 
     LaunchedEffect(key1 = transitionToRegisterScreen) {
         if (transitionToRegisterScreen) {
+            loginFormVisible = false
             registerButtonTransitionAnim.animateTo(1f, tween(400, easing = LinearOutSlowInEasing))
             registrationScreenVisible = true
             closeButtonTransitionAnim.animateTo(1f, tween(300))
+            registrationFormVisible = true
         } else {
+            registrationFormVisible = false
             closeButtonTransitionAnim.animateTo(0f, tween(300))
+            loginFormVisible = true
             registrationScreenVisible = false
             registerButtonTransitionAnim.animateTo(0f, tween(500))
         }
@@ -99,17 +115,51 @@ fun AuthScreen() {
             .fillMaxSize()
             .background(Color.LightGray.copy(alpha = 0.5f))
     ) {
+        val formPadding = 16.dp
+        val formPaddingInPixels = with(LocalDensity.current) { formPadding.toPx() }
+
+        val formTitleSize = 24.sp
+
+        val offset = lerp(0.dp, 10.dp, closeButtonTransitionAnim.value)
+        val offsetInPixels = with(LocalDensity.current) { offset.toPx() }
+
         Card(
             modifier = Modifier
                 .align(Alignment.Center)
                 .width(cardWidth)
-                .height(cardHeight),
+                .height(cardHeight)
+                .offset {
+                    IntOffset(
+                        x = offsetInPixels.toInt(),
+                        y = (-offsetInPixels).toInt()
+                    )
+                }
+                .graphicsLayer {
+                    alpha = (1.7 - closeButtonTransitionAnim.value).coerceAtMost(1.0).toFloat()
+                },
             colors = CardDefaults.cardColors(
                 containerColor = Color.White
             ),
             shape = RoundedCornerShape(8.dp),
             elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
         ) {
+            AnimatedVisibility(
+                visible = loginFormVisible,
+                enter = fadeIn(),
+                exit = fadeOut()
+            ) {
+                Column(
+                    modifier = Modifier.padding(formPadding)
+                ) {
+                    Text(
+                        text = "LOGIN",
+                        fontSize = formTitleSize,
+                        fontFamily = FontFamily.SansSerif,
+                        color = registrationBackgroundColor,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                }
+            }
 
         }
 
@@ -156,6 +206,24 @@ fun AuthScreen() {
 
                 }
 
+                AnimatedVisibility(
+                    enter = fadeIn(),
+                    exit = fadeOut(),
+                    visible = registrationFormVisible
+                ) {
+                    Column(
+                        modifier = Modifier.padding(formPadding)
+                    ) {
+                        Text(
+                            text = "REGISTER",
+                            fontSize = formTitleSize,
+                            fontFamily = FontFamily.SansSerif,
+                            color = Color.White,
+                            fontWeight = FontWeight.SemiBold
+                        )
+                    }
+                }
+
                 Image(
                     modifier = Modifier
                         .size(40.dp)
@@ -169,8 +237,8 @@ fun AuthScreen() {
                             val initialPosition = IntOffset(x, y.toInt())
 
                             val finalPosition = IntOffset(
-                                (cardWidthInPixels - registrationIconSizeInPixels.toInt() - 32).toInt(),
-                                (32).toInt()
+                                (cardWidthInPixels - registrationIconSizeInPixels.toInt() - formPaddingInPixels).toInt(),
+                                (formPaddingInPixels).toInt()
                             )
 
                             lerp(initialPosition, finalPosition, closeButtonTransitionAnim.value)
